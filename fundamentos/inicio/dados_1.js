909ec1357f6088d2555e52c406f5d6370cbec141
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-11-10 20:42:47 -0300

    Trabalhando com Dados #01

---
+++ fundamentos/inicio/dados_1.js
@@ -0,0 +1,12 @@
+var produto = "Caneta"
+var quantidade = 20
+var preco = 1.50
+var imposto = 0.50
+
+console.log(produto)
+console.log(quantidade)
+console.log(preco)
+console.log(imposto)
+
+produto = "Caneta BIC"
+console.log(produto)
