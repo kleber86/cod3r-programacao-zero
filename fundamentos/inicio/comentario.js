1dc5644905f99054ea6808b50dfd9691a9fbf11e
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-11-09 22:51:45 -0300

    Comentários de Código

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,11 @@
+// console.log é um comando para exibir algo na tela do computador
+console.log('Passo #1')
+console.log('Passo #2') // comentário de uma linha que não atrapalha a execução
+//console.log('Passo #3')
+
+
+/*
+ * Este é um comentário 
+ * de várias linhas. 
+ */
+console.log('Comentários')
