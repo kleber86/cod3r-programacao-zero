45045dc06229a51b6eadc61002ad3a45710f257c
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-11-10 21:51:53 -0300

    Trabalhando com Dados #03

---
+++ fundamentos/inicio/dados_3.js
@@ -0,0 +1,9 @@
+let idade = 10
+console.log(typeof idade) // number
+console.log(typeof 10) // number
+
+let salario = 4999.99
+console.log(typeof salario) // number
+
+let estaChovendo = false
+console.log(typeof estaChovendo) // boolean
