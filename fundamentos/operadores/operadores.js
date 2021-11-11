8ae193f894bebe06d817a3a13c6fd560ee6e12d3
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-11-11 19:45:13 -0300

    Operadores Aritméticos - Prática

---
+++ fundamentos/operadores/operadores.js
@@ -0,0 +1,13 @@
+// Valores Literais
+console.log(8 + 3)
+console.log(9 - 2)
+console.log(3 * 9)
+console.log(5 % 4)
+
+// usando variáveis
+let a = 5
+let b = 9
+console.log(a + b)
+console.log(a - b)
+console.log(a * 9)
+console.log(a % b)
