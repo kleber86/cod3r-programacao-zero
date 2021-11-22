1b54d0a20a4580f6045589d2142318f337ff425c
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-11-22 19:07:53 -0300

    Desafio Operadores Relacionais - Resposta

---
+++ fundamentos/operadores/relacionais_desafio.js
@@ -0,0 +1,16 @@
+let a = 1
+let b = 2
+
+// TRUE
+console.log(a < b)
+console.log(a <= b)
+console.log(a != b)
+console.log(a !== a)
+console.log(a + a < a)
+
+// FALSE
+console.log(a > b)
+console.log(a >= b)
+console.log(a == b)
+console.log(a === b)
+console.log(a + a !== b)
