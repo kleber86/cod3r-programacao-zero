926cde88ae76b6e12ff56435ebf67a8ce97f136f
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-11-21 22:01:51 -0300

    Operadores Relacionais - Prática #01

---
+++ fundamentos/operadores/relacionais_1.js
@@ -0,0 +1,18 @@
+// usando literais
+console.log(4 > 1)
+console.log(4 < 1)
+console.log(4 == 1)
+console.log(4 >= 1)
+console.log(4 <= 1)
+console.log(4 != 1)
+
+// usando variaveis
+var num1 = 4
+var num2 = 1
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 == num2)
+console.log(num1 >= num2)
+console.log(num1 <= num2)
+console.log(num1 != num2)
