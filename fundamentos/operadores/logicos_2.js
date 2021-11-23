e5cf76c7295cf2a20d4813f8fc8826afc2e751db
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-11-23 20:57:10 -0300

    Operadores Lógicos - Prática #02

---
+++ fundamentos/operadores/logicos_2.js
@@ -0,0 +1,15 @@
+let t1 = true
+let t2 = true
+
+let comprarTV50 = t1 && t2
+console.log('Vamos comprar a TV 50"?', comprarTV50)
+
+let comprarTV32 = t1 !== t2
+console.log('Vamos comprar a TV 32"?', comprarTV32)
+
+let tomarSoverte = t1 || t2
+console.log('Vamos tomar sorverte? ', tomarSoverte)
+
+let ficarEmcasa = !tomarSoverte
+console.log('Vamos ficar em casa?', ficarEmcasa)
+
