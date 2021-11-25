87af03ce04a8982f30e06ac718d44be39ab0dc63
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-11-24 21:21:33 -0300

    Desafio Operadores Lógicos - Resposta

---
+++ fundamentos/operadores/logicos_desafio.js
@@ -0,0 +1,14 @@
+// Duas expressões resultar em TRUE
+let verdadeira1 = (2 + 2) == (2 * 2)
+let verdadeira2 = true || true
+
+console.log(verdadeira1)
+console.log(verdadeira2)
+
+// Duas expressões resultar em FALSE
+
+let falso1 = (2 + 0) < (0 + 0)
+let falso2 = true && false
+
+console.log(falso1)
+console.log(falso2)
