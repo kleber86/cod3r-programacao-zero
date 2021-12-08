a226e2f8d47d844ca51238de6d5be387dd2a0547
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-08 20:10:51 -0300

    If - Prática #02

---
+++ fundamentos/controle/if_2.js
@@ -0,0 +1,10 @@
+const nota = 9
+const bomComportamento = true
+
+if (nota >= 7 && bomComportamento)
+  console.log('Parabéns!!')
+
+if (nota < 6 || !bomComportamento)
+  console.log('Que pena!!')
+
+console.log('Fim!')
