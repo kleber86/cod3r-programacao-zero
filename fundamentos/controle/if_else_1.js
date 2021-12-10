af4d0f7d76170ba6c98cb82586e1f98ae2b6b694
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-09 21:06:53 -0300

    If/Else - Prática #01

---
+++ fundamentos/controle/if_else_1.js
@@ -0,0 +1,14 @@
+const verdade = true
+const falso = false
+
+if(verdade){
+  console.log('Exec')
+}else{
+  console.log('Não Exec')
+}
+
+if(falso){
+  console.log('Não Exec')
+}else{
+  console.log('Exec')
+}
