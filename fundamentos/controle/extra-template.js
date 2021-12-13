b6f2f3fbfe76062a28e478aaf7a3a9ae5755b4d7
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-12 23:37:39 -0300

    Extra: Template String

---
+++ fundamentos/controle/extra-template.js
@@ -0,0 +1,6 @@
+const nome = 'Kleber'
+const sobreNome = 'Nascimento'
+const nomeCompleto = `Meu nome completo é ${nome} ${sobreNome}`
+
+console.log(nomeCompleto)
+console.log(`A soma de 1 + 1 é: ${1 + 1}`)
