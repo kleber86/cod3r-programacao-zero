49db5a2d58bc15fbf6452fe64326adb575fa5eb7
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-14 19:50:14 -0300

    Exemplo SEM Função

---
+++ fundamentos/funcoes/sem_funcao.js
@@ -0,0 +1,15 @@
+const d1 = 1
+const m1 = 2
+const a1 = 2000
+
+console.log(`Dia: ${d1}`)
+console.log(`Mês: ${m1}`)
+console.log(`Ano: ${a1}`)
+
+const d2 = 2
+const m2 = 2
+const a2 = 2002
+
+console.log(`Dia: ${d1}`)
+console.log(`Mês: ${m2}`)
+console.log(`Ano: ${a2}`)
