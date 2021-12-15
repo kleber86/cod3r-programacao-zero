0800eac44f82704aa50bf3b416e4cca58ac343d5
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-14 21:03:25 -0300

    Exemplo COM Função #02

---
+++ fundamentos/funcoes/com_funcao_2.js
@@ -0,0 +1,14 @@
+const d1 = 01
+const m1 = 02
+const a1 = 2000
+
+const d2 = 2
+const m2 = 2
+const a2 = 2222
+
+function exibirData(d, m, a) {
+  console.log(`${d}-${m}-${a}`)
+}
+
+exibirData(d1, m1, a1)
+exibirData(d2, m2, a2)
