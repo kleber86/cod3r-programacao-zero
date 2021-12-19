c3696bbd6ebeedd5762c917a3704442b756fc3ae
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-18 22:57:50 -0300

    Usando For com Array #01

---
+++ fundamentos/arrays/for_1.js
@@ -0,0 +1,10 @@
+const nums = [1, 2, 3, 4, 5, 6]
+
+for(let i = 0; i < nums.length; i++){
+  console.log(nums[i])
+}
+
+console.log('------------- 2 em 2 --------------')
+for(let i = 0; i < nums.length; i += 2){
+  console.log(nums[i])
+}
