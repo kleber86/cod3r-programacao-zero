47f4c6f04b96917a59174c4a2d262348335d1c76
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-19 11:57:04 -0300

    Métodos do Array #01

---
+++ fundamentos/arrays/metodos_1.js
@@ -0,0 +1,11 @@
+const numeros = [1, 2, 3]
+
+numeros.push(4)
+numeros2 = numeros.concat(5, 6, 7)
+
+console.log(numeros.join('-'))
+console.log(numeros)
+console.log(numeros2)
+console.log(numeros.includes(10))
+console.log(numeros.includes(1))
+console.log(numeros.indexOf(2))
