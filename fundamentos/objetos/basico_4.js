76506b961b75480c3044247b4a89f285225531e1
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-20 19:07:23 -0300

    Básico sobre Objeto #04

---
+++ fundamentos/objetos/basico_4.js
@@ -0,0 +1,10 @@
+const usuario = {
+  email: 'aluno@aluno.com.br'
+}
+
+console.log(usuario.email)
+console.log(usuario.nome)
+
+usuario.nome = 'Ana'
+console.log(usuario.nome)
+console.log(usuario)
