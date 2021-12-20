b07e740a28828c82939facd3b455117eb4a7d712
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-20 20:11:34 -0300

    Função e Objeto #01

---
+++ fundamentos/objetos/funcao_e_objeto_1.js
@@ -0,0 +1,16 @@
+function criarData(dia, mes, ano) {
+  return {
+    dia: dia,
+    mes: mes,
+    ano: ano,
+    exibirData: function() {
+      return `${this.dia}/${this.mes}/${this.ano}`
+    }
+  }
+}
+
+const d1 = criarData(10, 10, 2010)
+const d2 = criarData(02, 02, 2002)
+
+console.log(d1.exibirData())
+console.log(d2.exibirData())
